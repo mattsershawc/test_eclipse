fadc84a4f2455103eae0e471c2259057678564d1
mattsershawc <masato.sasho@gmail.com>
2020-10-27 16:59:36 +0900

    2nd time

--- test_git/src/test/Main.java
+++ test_git/src/test/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] ahgs) {
 		System.out.println("Hello GitHub!");
 		System.out.println("Added");
+		System.out.println("Added one more time");
 	}
 
 }
